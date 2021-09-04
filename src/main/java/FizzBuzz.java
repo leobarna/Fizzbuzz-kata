8ba133ce13234f7a7da875b83eb6b5b091c51e29
leobarna <leonoradli94@gmail.com>
2021-09-04 23:05:04 +0200

    Initial commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public static String counter(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(n);
+        }
+    }
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class FizzBuzzTest {
+    @Test
+    void shouldLeaveNormalNumbersAlone() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertThat(fizzbuzz.counter(1), equalTo("1"));
+    }
+
+    @Test
+    void shouldReturnFizzIfTheNumberIsDivisibleByThree(){
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertThat(fizzbuzz.counter(3), equalTo("Fizz"));
+    }
+
+    @Test
+    void shouldReturnBuzzIfTheNumberIsDivisibleByFive() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertThat(fizzbuzz.counter(5), equalTo("Buzz"));
+    }
+
+    @Test
+    void shouldReturnFizzBuzzIfTheNumberIsDivisibleByThreeAndFive() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertThat(fizzbuzz.counter(15), equalTo("FizzBuzz"));
+    }
+}
